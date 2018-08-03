d777b6fd45f8f120ad00eca393f00e75f398693e
Riddlez666 <foreignfive@gmail.com>
2018-08-03 16:41:32 -0700

    more commands

---
+++ commands/botreg.js
@@ -0,0 +1,60 @@
+const Discord = require("discord.js");
+const botSettings = require("../configs/config.json");
+const miscSettings = require("../configs/settings.json");
+const botChans = require("../configs/botchans.json");
+const prefix = miscSettings.prefix;
+var getJSON = require('get-json');
+var mysql = require('mysql');
+const Web3 = require("web3");
+// EtherGem web3
+var web3 = new Web3();
+web3.setProvider(new web3.providers.HttpProvider(miscSettings.web3provider));
+
+var con = mysql.createPool({
+  connectionLimit : 25,
+  host: botSettings.mysqlip,
+  user: botSettings.mysqluser,
+  password: botSettings.mysqlpass,
+  database: botSettings.mysqldb
+});
+
+exports.run = (client, message, args) => {
+
+  let address = args[0];
+  let ip = "192.168.1.66";
+  let balance = "0";
+  let isOnline = "Offline";
+  let author = message.author.id;
+  let user = message.author.username;
+  con.getConnection(function(err, connection) {
+    if (err) throw err; // not connected!
+    connection.query("SELECT userId FROM data WHERE userId = ?", author, function (err, result) {
+
+      if (err) console.log(err);
+      try {
+        let parsed = JSON.stringify(result);
+        let obj = JSON.parse(parsed);
+        let authorCheck = obj[0]["userId"];
+        if (authorCheck !== null) {
+          if (author == authorCheck) {
+            return message.reply("Sorry you have registered already.");
+            connection.release();
+          };
+        }
+      }catch(e){
+        console.log("ERROR ::",e)
+      }
+
+      connection.query('INSERT INTO data (userId, userName, balance, address, ip, isOnline) VALUES (?, ?, ?, ?, ?, ?)', [author, user, balance, address, ip, isOnline]);
+      message.reply("You have been added to the database.");
+      // When done with the connection, release it.
+      connection.release();
+
+
+      // Handle error after the release.
+      if (err) throw err;
+      // Don't use the connection here, it has been returned to the pool.
+    });
+  });
+
+}
---
+++ commands/setcheck.js
@@ -0,0 +1,55 @@
+const Discord = require("discord.js");
+const botSettings = require("../configs/config.json");
+const miscSettings = require("../configs/settings.json");
+const botChans = require("../configs/botchans.json");
+const prefix = miscSettings.prefix;
+var getJSON = require('get-json');
+var mysql = require('mysql');
+
+var con = mysql.createPool({
+  connectionLimit : 25,
+  host: botSettings.mysqlip,
+  user: botSettings.mysqluser,
+  password: botSettings.mysqlpass,
+  database: botSettings.mysqldb
+});
+
+exports.run = (client, message, args) => {
+  if(!message.member.hasPermission('ADMINISTRATOR')){
+    return message.channel.send("You cannot use '/setcheck' command");
+  }
+  con.getConnection(function(err, connection) {
+    if (err) throw err; // not connected!
+    const author = args[0];
+    const response = args[1];
+    connection.query("SELECT * FROM data WHERE userId = ?", author, function (err, result) {
+      if (!result) return message.reply("User Not Registered.");
+      let parsed = JSON.stringify(result);
+      let obj = JSON.parse(parsed);
+      connection.query(`UPDATE data SET shouldCheck = ? WHERE userId = ?`, [response, author]);
+      console.log("data updated");
+      const embed = new Discord.RichEmbed()
+        .setTitle("EGEM Discord Bot.")
+        .setAuthor("TheEGEMBot", miscSettings.egemspin)
+
+        .setColor(miscSettings.okcolor)
+        .setDescription("EGEM User Update:")
+        .setFooter(miscSettings.footerBranding, miscSettings.img32x32)
+        .setThumbnail(miscSettings.img32shard)
+
+        .setTimestamp()
+        .setURL("https://github.com/TeamEGEM/EGEM-Bot")
+        .addField("IP Check Updated to: ", response)
+
+        message.reply({embed})
+      connection.release();
+      try {
+
+      } catch (e) {
+        console.log(e);
+        connection.release();
+      }
+    })
+  })
+
+}
